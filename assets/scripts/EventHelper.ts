8ab0cdb9a56ca2578849064325691de9be2f3d61
Guru <sribaran.guru@bombayplay.com>
2022-10-13 02:58:10 +0530

    Demo branch

--- assets/scripts/EventHelper.ts
+++
@@ -1,7 +0,0 @@
-export class EventHelper {
-	static dispatchEvent(eventName: string, target: any, userData?: any) {
-		const event = new cc.Event.EventCustom(eventName, true);
-		event.setUserData(userData);
-		target.dispatchEvent(event);
-	}
-}
--- assets/scripts/PowerupTypes.ts
+++
@@ -1,7 +0,0 @@
-export enum PowerupTypes {
-	FOG,
-	MO_SPEED,
-	WALL_BOOM,
-	WALL_PCHKK,
-	HOLE_PLUG
-}
